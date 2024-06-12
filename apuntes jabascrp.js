63f160826a75b44b443c93482997b71b49deb495
CoolPenwin <alberto.romero.martin.93@gmail.com>
2024-06-12 14:11:32 +0200

    Create apuntes jabascrp.js

---
+++ apuntes jabascrp.js
@@ -0,0 +1,67 @@
+  
+  
+  
+                    ///VARIABLES///
+VAR         // variable global  *obsoleta 
+ 
+                //VARIABLES DE BLOQUE
+CONST       // "variable" NO MODIFICABLE
+LET         // variable modoficable
+            //tipos de variables{
+        String "hola",'mundo'//texto 
+        Boolean             // verdadero o falso /binario 1 o 0
+        Number             // ovbio mumeros 23 
+        Number float:0.23 // coma flotante / numero 
+                         // }
+
+                /// TERMINAL ///
+console.log   // imprime en la Terminal *util para inspeccionar codigo
+
+                //fallos
+null            //valor nulo 
+undefined       //valor infedinido
+NaN             //no es un numero / Not a Number
+
+
+                ///     operaciones ///
+          // aritmetica (MATEMATICAS)
+        +       //SUMA
+        -       //RESTA
+        *       //MILTIPLICACION
+        /       //dividir
+        %       // el resto de una division
+
+                    ///comparadores
+//      < >     // mayor/menor que 5>8=false
+        <= =>   // menor o igual/mayor o igual        
+        ==      //igual pero pueden ser distintos tipos del mismo valor 
+        ===     // extrictamente igual de tipo y valor  
+
+
+                ///     condiciones ///
+         if (condition) { //ENTONCES
+        // código a ejecutar si la condición es verdadera
+        } 
+      else {
+         // código a ejecutar si la condición es falsa
+        }
+
+
+                    /// logica
+        &&  //AND   /Y
+        ||  //OR    /ó
+        !   //NOT   /no negacion
+
+            //comparadores
+        // < >    mayor/menor que 5>8=false
+        <= =>    // menor/mayor o igual
+        
+        ==  //igual pero pueden ser distintos tipos del mismo valor 
+        === // extrictamente igual de tipo y valor  
+
+
+// funciones
+            function test(){
+                
+            }
+
