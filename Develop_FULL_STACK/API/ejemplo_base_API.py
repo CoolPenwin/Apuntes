3676d2836f898756c6ded5102a09dbc6ba5dcb89
CoolPenwin <coolPenwin@proton.me>
2024-09-10 20:26:20 +0200

    Update ejemplo_base_API.py

--- Develop_FULL_STACK/API/ejemplo_base_API.py
+++ Develop_FULL_STACK/API/ejemplo_base_API.py
@@ -1,22 +1,27 @@
-from flask import Flask , jsonify
-from alumnos import alumnos
+# Importamos Flask y jsonify para crear la aplicación y devolver respuestas en formato JSON
+from flask import Flask , jsonify 
+# Importamos la lista de alumnos desde un módulo llamado alumnos
+from alumnos import alumnos 
 
 
 
 
-
+# Creamos una instancia de la aplicación Flask
 app = Flask(__name__)
 
 
 
-
+# Definimos una ruta de prueba que responde a solicitudes GET
 @app.route("/prueba" , methods=["GET"])
 def prueba():
+    # Devolvemos un mensaje en formato JSON para verificar que la ruta funciona
     return jsonify({"msg" : "Ruta funcionando correctamente"})
 
 
+# Definimos una ruta para obtener la lista de alumnos que responde a solicitudes GET
 @app.route("/alumnos" , methods=["GET"])
 def traer_alumnos():
+    # Devolvemos un mensaje y la lista de alumnos en formato JSON
     return jsonify({"msg" : "Estos son los alumnos del curso" , "listado": alumnos}
 
 
@@ -24,6 +29,6 @@ def traer_alumnos():
 
 
 
-
+# AL FINAL  Ejecutamos la aplicación en modo debug en el puerto 5000
 if __name__ == "__main__":
     app.run(debug=True , port=5000)
